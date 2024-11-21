036810035b940f42b0d9b46217bffc1dd2e5d967
Aadarsh Soni <84452674+aadarsh-2004@users.noreply.github.com>
2024-11-21 17:42:02 +0530

    Update cryptoAPI.js

--- backend/API/cryptoAPI.js
+++ backend/API/cryptoAPI.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const fetchLiveCryptoData = async () => {
   try {
-    const response = await axios.get("https://api.binance.com/api/v3/ticker");
+    const response = await axios.get("https://api.binance.com/api/v3/ticker/24hr");
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error.message);
