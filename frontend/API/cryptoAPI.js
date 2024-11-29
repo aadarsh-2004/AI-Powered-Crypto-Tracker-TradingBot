ecfb7db9aee89ca9c72ec718ff96905739a9ce7f
Aadarsh Soni <84452674+aadarsh-2004@users.noreply.github.com>
2024-11-29 19:38:30 +0530

    Update cryptoAPI.js

--- frontend/API/cryptoAPI.js
+++ frontend/API/cryptoAPI.js
@@ -1,7 +1,8 @@
 
 //frontend 
 // Use fetch  data from your backend
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = "https://ai-powered-crypto-tracker.onrender.com"
 // const API_BASE_URL = "http://localhost:5000"; // Hardcoded for testing
 
 const fetchLiveCryptoData = async () => {
