a98787cc85ad04c22efc877613fa03fe164d7b05
aadarsh-2004 <2020aadarshsoni@gmail>
2024-11-30 17:16:08 +0530

    disable loading

--- frontend/AuthContext.jsx
+++ frontend/AuthContext.jsx
@@ -7,7 +7,7 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const auth = getAuth();
 
     useEffect(() => {
